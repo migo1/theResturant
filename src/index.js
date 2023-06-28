56238c0a38064130f0d72616272fb17fdc818c5f
Robert Migot <31877143+migo1@users.noreply.github.com>
2023-06-28 12:17:53 +0300

    Revert "Add header"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './style.css';
-import navigation from "./modules/navigation.js";
-import Logo from "./assets/images.jpeg";
-
-navigation();
-
-const myIMage = new Image();
-myIMage.src = Logo;
-myIMage.alt = "logo";
-myIMage.classList.add("top-logo");
-const imageContainer = document.querySelector(".nav-logo");
-imageContainer.appendChild(myIMage);
