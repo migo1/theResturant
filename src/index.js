c79c3d6615215665145aa9326affc2abbacaa698
migo1 <ertmigot@gmail.com>
2023-06-28 12:50:29 +0300

    add-navigation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import navigation from "./modules/navigation.js";
+import Logo from './assets/Restaurant-logo.png';
+
+navigation();
+const myIMage = new Image();
+myIMage.src = Logo;
+myIMage.alt = "logo";
+myIMage.classList.add("top-logo");
+const imageContainer = document.querySelector(".nav-logo");
+imageContainer.appendChild(myIMage);
