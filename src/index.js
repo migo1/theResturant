143feb746036940745deb143357db3c7dce62a03
migo1 <ertmigot@gmail.com>
2023-06-28 14:21:14 +0300

    remove comments

--- src/index.js
+++ src/index.js
@@ -23,28 +23,14 @@ footerImageContainer.appendChild(footerImage);
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
-    // Show a preloader while fetching the data
     const preloader = document.querySelector(".preloader");
     preloader.style.display = "flex";
-
-    // Fetch the meal data
     const data = await fetchMenuData();
-
-    // Hide the preloader after the data is fetched
     preloader.style.display = "none";
-
-    // Use the fetched data
-    // console.log("Returned data:", data.meals);
-
-    // Process the data or perform other operations
     data.meals.forEach((meal) => {
       menuItem(meal);
     });
   } catch (error) {
-    // Handle any errors that occurred during the API call
-    // console.error("Error:", error);
-
-    // Hide the preloader in case of an error
     const preloader = document.querySelector(".preloader");
     preloader.style.display = "none";
   }
